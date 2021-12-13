cd2e5e86d512efe24f3f5113467003a01b6e82d8
Charly_Angel <c.a.a.a.503@gmail.com>
2021-12-13 11:18:40 -0600

    feat: agregar componente product card

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,56 @@
+import { createContext, CSSProperties } from 'react';
+import styles from '../styles/styles.module.css';
+import { useProduct } from '../hooks/useProduct';
+import {
+  onChangeArgs,
+  ProductContextProps,
+  Product,
+  InitialValues,
+  ProductCardHandlers,
+} from '../interfaces';
+
+export interface ProductCardProps {
+  product: Product;
+  children: (args: ProductCardHandlers) => JSX.Element;
+  className?: string;
+  style?: CSSProperties;
+  onChange?: (args: onChangeArgs) => void;
+  value?: number;
+  initialValues?: InitialValues;
+}
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export const ProductCard = ({
+  children,
+  product,
+  className,
+  style,
+  onChange,
+  value,
+  initialValues,
+}: ProductCardProps) => {
+  const { counter, increaseBy, maxCount, isMaxCountReached, reset } =
+    useProduct({
+      onChange,
+      product,
+      value,
+      initialValues,
+    });
+
+  return (
+    <Provider value={{ product, counter, increaseBy, maxCount }}>
+      <div className={`${styles.productCard} ${className}`} style={style}>
+        {children({
+          count: counter,
+          isMaxCountReached,
+          maxCount: initialValues?.maxCount,
+          product,
+          increaseBy,
+          reset,
+        })}
+      </div>
+    </Provider>
+  );
+};
---
+++ src/components/ProductImage.tsx
@@ -0,0 +1,26 @@
+import { CSSProperties, useContext } from 'react';
+import { ProductContext } from './ProductCard';
+import NoImage from '../assets/no-image.jpg';
+import styles from '../styles/styles.module.css';
+
+export interface Props {
+  image?: string;
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const ProductImage = ({ image, className, style }: Props) => {
+  const { product } = useContext(ProductContext);
+  let imgToShow: string;
+
+  imgToShow = image ? image : product.img ? product.img : NoImage;
+
+  return (
+    <img
+      className={`${styles.productImg} ${className}`}
+      style={style}
+      src={imgToShow}
+      alt='product'
+    />
+  );
+};
